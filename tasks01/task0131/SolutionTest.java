94824207c9786febce87080215eaf7c91259331e
Nikita Golovan <nikita.golovan.00@gmail.com>
2022-07-05 12:01:58 +0300

    first task

---
+++ tasks01/task0131/SolutionTest.java
@@ -0,0 +1,15 @@
+package tasks01.task0131;
+
+import org.junit.Test;
+import tasks01.task0131.Solution;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void test1() {
+        assertEquals(2, Solution.getMetreFromCentimetre(270));
+    }
+
+
+}
