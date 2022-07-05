2aad06e473c4b6d559df3f5dec59e4eb2a4aa9c4
RichardReporting <108662175+RichardReporting@users.noreply.github.com>
2022-07-05 10:33:29 +0300

    Delete task0131 directory

--- task0131/MainTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class MainTest {
-    @Test
-    public void test1() {
-        assertEquals(2,Main.getMetreFromCentimetre(270));
-    }
-}
