ac461151ac4709cea095822cdc149be5bac5a3ff
Nikita Golovan <nikita.golovan.00@gmail.com>
2022-07-07 11:12:54 +0300

    finished lvl2

--- tasks01/task0133/Solution.java
+++ tasks01/task0133/Solution.java
@@ -3,11 +3,12 @@ package tasks01.task0133;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
     }
-    public static  int getCountSecondsPassedInCurrentHour(int seconds){
-        return(seconds%3600);
+
+    public static int getCountSecondsPassedInCurrentHour(int seconds) {
+        return (seconds % 3600);
     }
 }
