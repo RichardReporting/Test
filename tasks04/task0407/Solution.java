3b1a5764f499f9fceda87381fa01ccf7aebc18be
Nikita Golovan <nikita.golovan.00@gmail.com>
2022-07-18 18:06:59 +0300

    finished lvl3

--- tasks04/task0407/Solution.java
+++ tasks04/task0407/Solution.java
@@ -1,16 +1,19 @@
 package tasks04.task0407;
 
 public class Solution {
-    public static void main(String[] args) {
-        Cat cat1 = new Cat();
-        Cat.count++;
+    static int count = 0;
 
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat();
         Cat cat2 = new Cat();
-        Cat.count++;
-        System.out.print(Cat.count);
+        System.out.print(count);
     }
 
     public static class Cat {
-        public static int count = 0;
+
+        Cat() {
+            count++;
+        }
     }
 }
